39b632cae398971858f1b5d0c4fdc9ced460b704
Amiola Demilade <amiolademilade@gmail.com>
2025-05-07 12:32:59 +0100

    cors-iisue

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRoute from './routes/auth-route';
 import dotenv from "dotenv";
+import cors from 'cors'
 
 dotenv.config();
 
@@ -8,7 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Root route
 app.get('/', (_req, res) => {
